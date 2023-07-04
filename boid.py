add2547b4180af7c3f843e4c1781c24d43123eea
dariocazzani <dariocazzani@gmail.com>
2023-07-04 14:24:56 -0400

    It seems to kind of work

--- boid.py
+++ boid.py
@@ -3,24 +3,49 @@ import numpy as np
 import random
 
 def set_magnitude(vector:np.ndarray, magnitude:float) -> np.ndarray:
+    norm = np.linalg.norm(vector)
+    if norm < 1e-3:
+        return vector
     normalized_vector = vector / np.linalg.norm(vector)
     scaled_vector = normalized_vector * magnitude
     return scaled_vector
 
+def average_heading(vectors:np.ndarray) -> np.ndarray:
+    sum_vector = np.sum(vectors, axis=0)
+    heading = sum_vector / np.linalg.norm(sum_vector)  
+    return heading
+
+def average_position(vectors:np.ndarray) -> np.ndarray:
+    return np.mean(vectors, axis=0)
+
+def limit_magnitude(vector: np.ndarray, max_magnitude: float) -> np.ndarray:
+    norm = np.linalg.norm(vector)
+    if norm < 1e-3:
+        return vector  
+    if norm > max_magnitude:
+        normalized_vector = vector / norm
+        scaled_vector = normalized_vector * max_magnitude
+        return scaled_vector
+    else:
+        return vector
+
 
 class Boid:
-    def __init__(self, screen_height:int=1000, screen_width:int=1000) -> None:
+    def __init__(self, screen_height:int, screen_width:int) -> None:
         self.s_heigh = screen_height
         self.s_width = screen_width
         self.position = np.array([random.uniform(0, screen_width), random.uniform(0, screen_height)])
-        self.velocity = np.random.randn(2) * 4
+        self.velocity = np.random.randn(2) * 2
         self.acceleration = np.array([0.0, 0.0])
-        self.max_force:float = 0.2
-        self.perception:float = 100
+        self.max_force:float = 0.5
+        self.perception:float = 50
+        self.max_speed:float = 5
         
     def update(self) -> None:
         self.position += self.velocity
         self.velocity += self.acceleration
+        self.velocity = limit_magnitude(self.velocity, self.max_speed)
+        self.acceleration = np.zeros_like(self.acceleration)
         
     def edges(self) -> None:
         if self.position[0] > self.s_width:
@@ -32,24 +57,78 @@ class Boid:
         elif self.position[1] < 0:
             self.position[1] = self.s_heigh
             
+    def toroidal_distance(self, pos1, pos2, width, height):
+        dx = abs(pos1[0] - pos2[0])
+        dy = abs(pos1[1] - pos2[1])
+        dx = min(dx, width - dx)
+        dy = min(dy, height - dy)
+        return np.sqrt(dx**2 + dy**2)
+            
         
     def _align(self, boids:list['Boid']) -> np.ndarray:
         other_velocities = []
         for other in boids:
-            if self != other and \
-                np.linalg.norm(self.position - other.position) < self.perception:
+            distance = self.toroidal_distance(self.position, other.position, self.s_width, self.s_heigh)
+            if self != other and distance < self.perception:
                 other_velocities.append(other.velocity)
-        steering = np.mean(np.array(other_velocities), axis=0) if len (other_velocities) > 0 else np.zeros_like(self.velocity)
-        return set_magnitude(steering - self.velocity, self.max_force)
-    
-    def flock(self, boids) -> None:
+        
+        if len(other_velocities) > 0:       
+            steering_force = average_heading(np.array(other_velocities)) - self.velocity
+            steering_force = limit_magnitude(steering_force, self.max_force)
+            return steering_force
+        else:
+            return np.zeros_like(self.acceleration)   
+        
+    def _cohesion(self, boids:list['Boid']) -> np.ndarray:
+        other_positions = []
+        for other in boids:
+            distance = self.toroidal_distance(self.position, other.position, self.s_width, self.s_heigh)
+            if self != other and distance < self.perception:
+                other_positions.append(other.position)
+                
+        if len(other_positions) > 0:
+            steering_force = average_position(np.array(other_positions)) - self.position
+            steering_force = steering_force - self.velocity
+            steering_force = limit_magnitude(steering_force, self.max_force)
+            return steering_force
+        else:
+            return np.zeros_like(self.velocity)
+        
+    def _separation(self, boids:list['Boid']) -> np.ndarray:
+        new_vec = []
+        for other in boids:
+            distance = self.toroidal_distance(self.position, other.position, self.s_width, self.s_heigh)
+            if self != other and distance < self.perception:
+                tmp = self.position - other.position
+                tmp = tmp / (distance + 1e-2)
+                new_vec.append(tmp)
+        if len(new_vec) > 0:
+            steering_force = np.mean(np.array(new_vec), axis=0)
+            steering_force = steering_force - self.velocity
+            steering_force = limit_magnitude(steering_force, self.max_force)
+            return steering_force
+        else:
+            return np.zeros_like(self.velocity)
+                
+                
+    def flock(self, boids, align_mult, cohesion_mult, sep_mult) -> None:
         alignment = self._align(boids)
-        self.acceleration = alignment
+        cohesion = self._cohesion(boids)
+        separation = self._separation(boids)
+        
+        alignment *= align_mult
+        cohesion *= cohesion_mult
+        separation *= sep_mult
+
+        self.acceleration += alignment
+        self.acceleration += cohesion
+        self.acceleration += separation
+    
     
     def show(self, screen):
         # pygame.draw.circle(surface, color, position, radius)
         color:tuple = (255, 255, 255)
-        radius:float = 10
+        radius:float = 5
         pygame.draw.circle(
             screen,
             color,
--- run.py
+++ run.py
@@ -1,36 +1,111 @@
 import pygame
+import pygame_gui
 from boid import Boid
 
 
-pygame.init()
-width, height = 1000, 1000
-screen_color = (0, 0, 0)
-screen = pygame.display.set_mode((width, height))
+class Settings:
+    SCREEN_WIDTH = 1000
+    SCREEN_HEIGHT = 1000
+    SCREEN_COLOR = (0, 0, 0)
+    CLOCK_TICK = 60
+
+
+def create_gui_elements(manager):
+    gui_elements = {}
+    gui_elements["align_slider"] = pygame_gui.elements.UIHorizontalSlider(
+        relative_rect=pygame.Rect((120, 20), (200, 20)),
+        start_value=1.1,
+        value_range=(0.0, 5.0),
+        manager=manager
+    )
+    gui_elements["align_label"] = pygame_gui.elements.UILabel(
+        relative_rect=pygame.Rect((20, 20), (100, 20)),
+        text="Alignment:",
+        manager=manager
+    )
+    gui_elements["cohesion_slider"] = pygame_gui.elements.UIHorizontalSlider(
+        relative_rect=pygame.Rect((120, 50), (200, 20)),
+        start_value=0.25,
+        value_range=(0.0, 0.5),
+        manager=manager
+    )
+    gui_elements["cohesion_label"] = pygame_gui.elements.UILabel(
+        relative_rect=pygame.Rect((20, 50), (100, 20)),
+        text="Cohesion:",
+        manager=manager
+    )
+    gui_elements["separation_slider"] = pygame_gui.elements.UIHorizontalSlider(
+        relative_rect=pygame.Rect((120, 80), (200, 20)),
+        start_value=0.25,
+        value_range=(0.0, 0.5),
+        manager=manager
+    )
+    gui_elements["separation_label"] = pygame_gui.elements.UILabel(
+        relative_rect=pygame.Rect((20, 80), (100, 20)),
+        text="Separation:",
+        manager=manager
+    )
+    gui_elements["restart_button"] = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((120, 120), (100, 20)),
+        text="Restart",
+        manager=manager
+    )
+    return gui_elements
+
+
+def handle_events(event, manager, gui_elements):
+    global flock
+    manager.process_events(event)
+    if event.type == pygame.USEREVENT:
+        if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+            if event.ui_element == gui_elements["restart_button"]:
+                flock = [Boid(screen_height=Settings.SCREEN_HEIGHT, screen_width=Settings.SCREEN_WIDTH) for _ in range(100)]
 
 
 def main():
-    flock = [Boid() for _ in range(100)]
-    boid = Boid()
+    pygame.init()
+    screen = pygame.display.set_mode((Settings.SCREEN_WIDTH, Settings.SCREEN_HEIGHT))
+
+    manager = pygame_gui.UIManager((Settings.SCREEN_WIDTH, Settings.SCREEN_HEIGHT))
+    gui_elements = create_gui_elements(manager)
+
+    global flock
+    flock = [Boid(screen_height=Settings.SCREEN_HEIGHT, screen_width=Settings.SCREEN_WIDTH) for _ in range(100)]
+
     running = True
+    clock = pygame.time.Clock()
+
     while running:
+        time_delta = clock.tick(Settings.CLOCK_TICK) / 1000.0
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-                
-        # Clear the screen (fill with black color)
-        screen.fill(screen_color)
-        
+            handle_events(event, manager, gui_elements)
+
+        manager.update(time_delta)
+
+        gui_elements["align_label"].set_text(f'A: {gui_elements["align_slider"].get_current_value():.2f}')
+        gui_elements["cohesion_label"].set_text(f'C: {gui_elements["cohesion_slider"].get_current_value():.2f}')
+        gui_elements["separation_label"].set_text(f'S: {gui_elements["separation_slider"].get_current_value():.2f}')
+
+        align_mult = gui_elements["align_slider"].get_current_value()
+        cohesion_mult = gui_elements["cohesion_slider"].get_current_value()
+        sep_mult = gui_elements["separation_slider"].get_current_value()
+
+        screen.fill(Settings.SCREEN_COLOR)
+
         for boid in flock:
+            boid.flock(flock, align_mult, cohesion_mult, sep_mult)
             boid.edges()
-            boid.flock(flock)
             boid.update()
             boid.show(screen)
-        
+
+        manager.draw_ui(screen)
         pygame.display.flip()
-        pygame.time.delay(33)
-    
+
     pygame.quit()
-    pass
+
 
 if __name__ == "__main__":
     main()
